4b55228605b4588d60094884393ad277ce2c4139
JYJ <jyj13757628750@163.com>
2024-07-10 17:35:50 +0800

    test

--- result_DHF1k.py
+++ result_DHF1k.py
@@ -6,7 +6,7 @@ import numpy as np
 from PIL import Image
 import time
 from utils import *
-from models.TSYNet9 import TSYNet
+from models.TSYNet import TSYNet
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
